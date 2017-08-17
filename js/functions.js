9c26528e08f3588b1bcb61e30108a83ac88a6705
Jordan Leslie <this.codedev@gmail.com>
2017-08-17 16:53:06 -0500

    Add functions exercise solutions

---
+++ js/functions.js
@@ -0,0 +1,73 @@
+"use strict";
+console.log("Connected");
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ *  > sayHello("codeup") // returns "Hello, codeup!"
+ */
+function sayHello(name){
+    return("Hello, " + name + "!");
+}
+/**
+ * TODO:
+ * Call the function 'sayHello' and pass your name as a string literal argument.
+ * Store the result of the function call in a variable named 'helloMessage'.
+ * console.log 'helloMessage' to check your work
+ * Now store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+
+var helloMessage = sayHello("Jordan");
+
+// Don't modify the following line, it generates a random number between 1 and
+// 100 and stores it in a variable named random
+var random = Math.floor((Math.random() * 100) + 1);
+
+/**
+ * TODO:
+ * Create a function called 'isOdd' that takes a number as a parameter.
+ * The function should use the ternary operator to return a message.
+ * The message should contain the number being checked, and whether or not the
+ * number is odd
+ *
+ * Example
+ *  > isOdd(42) // returns "42 is not odd!"
+ *
+ * Call the function 'isOdd' passing the variable 'random' as a parameter.
+ * console.log *outside of the function* to check your work
+*/
+function isOdd(num) {
+    return num % 2 === 0 ? console.log(num + " is even!") : console.log(num + " is odd!");
+}
+/**
+
+ * TODO: Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant
+ *
+ * the function should accept a tip percentage and the total of the bill, and
+ * return the amount to tip
+ *
+ * Example
+ *  > calculateTip(0.20, 20) // returns 4
+ */
+function calculateTip(bill, tip){
+
+    tip = bill * tip;
+    return tip;
+
+}
+/**
+ * TODO: use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+function getTip() {
+    var tip = Number(prompt("Enter the tip percent as a decimal value."));
+    var bill = Number(prompt("Enter the amount of the bill."));
+
+    return bill * tip;
+}
